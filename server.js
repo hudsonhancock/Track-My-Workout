1db660645dc31014b3a1a9f8627208559f5124f7
Hudson Hancock <hudsonhancock@gmail.com>
2021-10-27 20:17:55 -0400

    Adds routes

--- server.js
+++ server.js
@@ -10,6 +10,18 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+}); //this is the homepage
+
+app.get("/exercise", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/exercise.html"));
+}); //this is the excercise page
+
+app.get("/stats", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/stats.html"));
+}); //this is the stats page
+
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness", {
   useNewUrlParser: true,
 });
