e4d5768271d68fba0646acd2987974bb0db176ec
Hudson Hancock <hudsonhancock@gmail.com>
2021-11-10 21:14:46 -0500

    Adds css styling to index.html

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path");
 const mongoose = require("mongoose");
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
